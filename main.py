d021730fb25b020ba8eba735431c8adcbc0be8e7
Ravitejakolanu <148851810+Ravitejakolanu@users.noreply.github.com>
2025-10-30 19:05:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from faster_whisper import WhisperModel
 
 app = FastAPI()
 
-# Lightweight model for free Render
 model = WhisperModel("tiny", device="cpu")
 
 @app.post("/transcribe")
@@ -14,7 +13,12 @@ async def transcribe(audio: UploadFile):
     with open(file_name, "wb") as f:
         f.write(contents)
 
-    segments, info = model.transcribe(file_name, language="te", task="translate")
+    segments, info = model.transcribe(
+        file_name, 
+        language="te", 
+        task="translate",
+        temperature=0
+    )
     text = " ".join([seg.text for seg in segments])
 
     return {"text": text}
